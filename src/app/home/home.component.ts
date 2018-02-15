f9459c71b62e6facbf73192176f24c7a0bf8bdb8
Eric Alter <neolanders@hotmail.com>
2018-02-15 09:36:00 -0800

    Working progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../shared';
 import { HeaderService } from '../shared/services/header.service';
 
@@ -11,18 +11,15 @@ export class HomeComponent implements OnInit {
   public isAuthenticated: boolean;
 
   constructor(
-    private router: Router,
+    private route: ActivatedRoute,
     private userService: UserService,
     private headerService: HeaderService
   ) {}
 
   ngOnInit(): void {
     this.headerService.setTitle('Home');
-    this.userService.checkIsAuthenticated()
-    .subscribe(
-      authenticated => {
-        this.isAuthenticated = authenticated;
-      }
-    );
+    this.route.data.subscribe((data: { isAuthenticated: boolean }) => {
+      this.isAuthenticated = data.isAuthenticated;
+    });
   }
 }
--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -2,5 +2,4 @@ export * from './user.service';
 export * from './api.service';
 export * from './auth.guard';
 export * from './jwt.service';
-export * from './user.service';
 export * from './header.service';
